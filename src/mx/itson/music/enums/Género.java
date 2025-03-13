f5142ba2cfe5f6e0fc975fc036d18a2fa1ac943f
jcecena <jantonio.cecena@gmail.com>
2025-03-13 09:56:06 -0700

    Se agregaron las clases de entidad acorde al modelo.

---
+++ src/mx/itson/music/enums/Género.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package mx.itson.music.enums;
+
+/**
+ *
+ * @author janto
+ */
+public enum Género {
+    POP,
+    ROCK,
+    DANCE,
+    SALSA,
+    OTRO
+}
